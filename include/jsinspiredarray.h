0a6ea2ede152a393ead49a6cbfe210f0464cc7e9
jazminbelmonte <jazminbelmonte@gmail.com>
2018-10-02 16:07:52 -0600

    rough draft commit

--- include/jsinspiredarray.h
+++ include/jsinspiredarray.h
@@ -14,12 +14,84 @@ struct Node {
 
 template <typename T>
 class LinkedListIterator{
-  // TODO
+
+public:
+  // TODO: LinkedListOperator(Node<T>*)
+  LinkedListIterator(Node<T>* ptr): current(ptr){}
+
+  // TODO: Operator++
+  LinkedListIterator<T>& operator++(){
+    current = current->link;
+    return *this;
+  }
+
+  // TODO: Operator*
+  T operator*(){
+    return current->info;
+  }
+
+  // TODO: Operator==
+  bool operator==(const LinkedListIterator<T>& rhs) {
+    return current == rhs.current;
+  }
+
+  // TODO: Operator!=
+  bool operator!=(const LinkedListIterator<T>& rhs) {
+    return current != rhs.current;
+  }
+
+private:
+  // TODO: current
+  Node<T>* current;
+
 };
 
 template <typename T>
 class JSInspiredArray {
-  // TODO
+
+public:
+  // TODO: JSInspiredArray()
+  JSInspiredArray(): count(0), first(nullptr), last(nullptr){}
+
+  // TODO: JSInspiredArray(const JSInspiredArray<T>&)
+  JSInspiredArray(const JSInspiredArray<T>& array): JSInspiredArray(){
+    auto current = array.first;
+    while (current){
+      add(current->info);
+      current = current->link;
+    }
+  }
+
+  // TODO: operator=(const JSInspiredArray<T>&)
+
+  // TODO: push(T)
+
+  // TODO: pop(T)
+
+  // TODO: unshift(T)
+
+  // TODO: shift()
+
+  // TODO: concat(const JSInspiredArray<T>&)
+
+  // TODO: find(T)
+
+  // TODO: friend ostream <<
+
+  // TODO: empty()
+
+  // TODO: length()
+
+  // TODO: begin()
+
+  // TODO: end()
+
+  // TODO: ~JSInspiredArray()
+
+protected:
+    Node<T>* first;
+    Node<T> last;
+    unsigned count;
 };
 
 #endif
